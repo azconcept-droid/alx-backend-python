831f7af65e1cd7d21257c228a2a8dfbefc93ada5
Yahaya Azeez <azconcept2016@gmail.com>
2024-08-03 13:01:19 +0100

    fix pycodestyle

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-""" Module that annotate a given function 
+""" Module that annotate a given function
 """
 
 from typing import Iterable, Sequence, List, Tuple
+
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """ Return item and length of the item in tuple
