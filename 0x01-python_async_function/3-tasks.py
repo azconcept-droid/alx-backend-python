7970cf950a4892dcbfa0a306ca5f588440b59985
Yahaya Azeez <azconcept2016@gmail.com>
2024-08-05 16:14:10 +0100

    change variable name and return type

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -6,13 +6,13 @@ from typing import Awaitable
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> Awaitable:
+def task_wait_random(max_delay: int) -> Awaitable[float]:
     """Create task and
     return asyncio.task for wait_random function
     """
 
-    # chedule wait_random() to run soon concurrently
+    # Schedule wait_random() to run soon concurrently
     # with "task_wait_random".
-    task1 = asyncio.create_task(wait_random(max_delay))
+    task = asyncio.create_task(wait_random(max_delay))
 
-    return task1
+    return task
