25dd0588d32e9eace5c6f70c9a2fb5402ec64f51
Yahaya Azeez <azconcept2016@gmail.com>
2024-08-05 14:49:02 +0100

    generate list of delay time

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,3 +1,18 @@
 #!/usr/bin/env python3
+""" Concurrent coroutines module
 """
-"""
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """ wait for n times with max_delay
+    """
+    delay: float
+    delays: List[float] = []
+
+    for _ in range(n):
+        delay = await wait_random(max_delay)
+        delays.append(delay)
+
+    return delays
