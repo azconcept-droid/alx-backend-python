b76edd80cdeecd50a45ffd2b354fb64c562a53b5
Yahaya Azeez <azconcept2016@gmail.com>
2024-08-05 22:48:15 +0100

    change return type

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -2,17 +2,15 @@
 """ Asyncio task module
 """
 import asyncio
-from typing import Awaitable
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> Awaitable[float]:
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """Create task and
     return asyncio.task for wait_random function
     """
 
     # Schedule wait_random() to run soon concurrently
-    # with "task_wait_random".
     task = asyncio.create_task(wait_random(max_delay))
 
     return task
