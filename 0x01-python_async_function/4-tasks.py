5a6e04769ab6e461fa1a690fb66564402f7fd830
Yahaya Azeez <azconcept2016@gmail.com>
2024-08-05 16:08:25 +0100

    get list of tasks time

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,3 +1,18 @@
 #!/usr/bin/env python3
+""" Concurrent coroutines module
 """
-"""
+from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """ wait for n times with max_delay
+    """
+    delay: float
+    delays: List[float] = []
+
+    for _ in range(n):
+        delay = await task_wait_random(max_delay)
+        delays.append(delay)
+
+    return delays
