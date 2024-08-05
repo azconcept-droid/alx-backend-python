6c3455059820a817c08a621786606b0f92d1debb
Yahaya Azeez <azconcept2016@gmail.com>
2024-08-05 15:58:16 +0100

    create task with asyncio

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,3 +1,18 @@
 #!/usr/bin/env python3
+""" Asyncio task module
 """
-"""
+import asyncio
+from typing import Awaitable
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> Awaitable:
+    """Create task and
+    return asyncio.task for wait_random function
+    """
+
+    # chedule wait_random() to run soon concurrently
+    # with "task_wait_random".
+    task1 = asyncio.create_task(wait_random(max_delay))
+
+    return task1
