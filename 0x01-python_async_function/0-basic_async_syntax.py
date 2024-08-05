e5be261989a6a6add4ef6c9c47462f7f2e598089
Yahaya Azeez <azconcept2016@gmail.com>
2024-08-05 12:55:36 +0100

    generate random number from 0 to N included

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python3
+""" Basic aync module
 """
-"""
+from random import uniform
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """ asynchronous coroutine
+    that takes in an integer argument max_delay
+    that waits for a random delay between 0 and max_delay
+    (included and float value) seconds and eventually returns it
+    """
+
+    return uniform(0, max_delay)
