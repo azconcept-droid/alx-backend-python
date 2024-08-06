04a54324f2dd3ec7b2a46ae1ca803e386458aa4f
Yahaya Azeez <azconcept2016@gmail.com>
2024-08-06 12:51:04 +0100

    generator that yeild random num 0-10

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,3 +1,16 @@
 #!/usr/bin/env python3
+""" Async generator module
 """
-"""
+import random
+import asyncio
+from typing import AsyncGenerator, Any
+
+
+async def async_generator() -> AsyncGenerator[float, Any]:
+    """ Generator function
+    """
+
+    for _ in range(10):
+        rand_num: float = random.uniform(0, 10)
+        yield rand_num
+        await asyncio.sleep(1)
