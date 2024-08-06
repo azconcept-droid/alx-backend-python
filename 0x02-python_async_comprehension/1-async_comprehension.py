911f28025165f7783c34e03615cd99b22501293e
Yahaya Azeez <azconcept2016@gmail.com>
2024-08-06 13:05:49 +0100

    generate random numbers in a list

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3
+""" Async comprehensions module
 """
-"""
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """ Generate random number using async comprehensions
+    """
+
+    return [i async for i in async_generator()]
