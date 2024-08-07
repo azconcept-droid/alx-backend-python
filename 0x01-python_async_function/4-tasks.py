967eaae4c7012d1042413d8c2b0ced5995fd034d
Yahaya Azeez <azconcept2016@gmail.com>
2024-08-07 15:37:32 +0100

    return result in ascending other

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ Concurrent coroutines module
 """
+import asyncio
 from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
@@ -8,13 +9,13 @@ task_wait_random = __import__('3-tasks').task_wait_random
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """ wait for n times with max_delay
     """
-    delay: float
+    # delays: list of float
     delays: List[float] = []
 
-    index = 0
-    while index < n:
-        delay = await task_wait_random(max_delay)
-        delays.append(delay)
-        index += 1
+    # collect tasks with respect to time to finish
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+
+    # collect returned tasks result in ascending other
+    delays = [await task for task in asyncio.as_completed(tasks)]
 
     return delays
