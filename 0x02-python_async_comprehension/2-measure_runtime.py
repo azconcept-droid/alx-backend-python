4ef6c26c4e4380fc0846dd535d45942f04594294
Yahaya Azeez <azconcept2016@gmail.com>
2024-08-07 14:16:10 +0100

    fix pycodestyle E128 continuation line under-indented for visual indent

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -15,7 +15,7 @@ async def measure_runtime() -> float:
 
     start_time: float = time.perf_counter()
     await asyncio.gather(async_comprehension(), async_comprehension(),
-                   async_comprehension(),  async_comprehension())
+                         async_comprehension(),  async_comprehension())
     total_time = time.perf_counter() - start_time
 
     return total_time
