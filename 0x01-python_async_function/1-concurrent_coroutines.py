d5ff9cb4cd62777af77f15a878a4da0b753b77c1
Yahaya Azeez <azconcept2016@gmail.com>
2024-08-07 15:23:17 +0100

    sort list in ascending other

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ Concurrent coroutines module
 """
+import asyncio
 from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
@@ -8,13 +9,13 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """ wait for n times with max_delay
     """
-    delay: float
+    # delays: list of float
     delays: List[float] = []
 
-    index = 0
-    while index < n:
-        delay = await wait_random(max_delay)
-        delays.append(delay)
-        index += 1
+    # collect tasks with respect to time to finish
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+
+    # collect returned tasks in ascending other
+    delays = [await task for task in asyncio.as_completed(tasks)]
 
     return delays
