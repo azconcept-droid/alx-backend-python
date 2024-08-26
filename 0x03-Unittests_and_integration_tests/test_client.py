eae24f9b2d748bccb7a0168ba548ec2c1357e2d9
Yahaya Azeez <azconcept2016@gmail.com>
2024-08-26 18:29:36 +0100

    transfer code to appropriate file

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,25 +1,3 @@
 #!/usr/bin/env python3
 """ Parameterize and patch as decorators
 """
-import unittest
-from utils import access_nested_map
-from parameterized import parameterized
-from typing import (
-    Mapping,
-    Sequence,
-    Any,
-)
-
-
-class TestAccessNestedMap(unittest.TestCase):
-    """ Class to test access nested map """
-
-    @parameterized.expand([
-        ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2),
-    ])
-    def test_access_nested_map(self, nested_map: Mapping,
-                               path: Sequence, expected: Any):
-        """ method to test access nested map """
-        self.assertEqual(access_nested_map(nested_map, path), expected)
